9c989ce1c4bd5de48beeefb6a5beda268e0f48a3
Maxime Peralez <Maxime@ip-213-124-172-254.ip.prioritytelecom.net>
2017-03-25 18:26:29 +0100

    Completed setting up Firebase integration

---
+++ SocialNetwork/SignInVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SocialNetwork
+//
+//  Created by Maxime Peralez on 25/03/2017.
+//  Copyright © 2017 Maxime Peralez. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    
+    }
+
+
+}
+
