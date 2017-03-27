e44f9b5b09a927a595749a0f3539c7b6abcdfcc2
Maxime Peralez <Maxime@ip-213-124-172-254.ip.prioritytelecom.net>
2017-03-27 13:53:40 +0200

    Facebook authentication enabled

--- SocialNetwork/SignInVC.swift
+++ SocialNetwork/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
     
@@ -15,6 +18,37 @@ class SignInVC: UIViewController {
         
     }
 
+    @IBAction func facebookBtnPressed(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            
+            if error != nil {
+                print("MAX : Unable to authenticate with Facebook - \(error)")
+            } else if result?.isCancelled == true {
+                print("MAX : User cancelled Facebook authentication")
+            } else {
+                print("MAX : Succesfully authenticated with Facebook")
+                
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        
+        FIRAuth.auth()?.signIn(with: credential, completion: { (result, error) in
+            
+            if error != nil {
+                print("MAX : Unable to authenticate with Firebase - \(error)")
+            } else {
+                print("MAX : Succesfully authenticated with Firebase")
+            }
+        })
+    }
 
 }
 
