a618eabf706ed298f7e0bca0338edb55b9ed8e56
Maxime Peralez <Maxime@ip-213-124-172-254.ip.prioritytelecom.net>
2017-03-28 15:57:04 +0200

    Completed creating Firebase DB users

--- SocialNetwork/SignInVC.swift
+++ SocialNetwork/SignInVC.swift
@@ -58,7 +58,8 @@ class SignInVC: UIViewController {
             } else {
                 print("MAX : Succesfully authenticated with Firebase")
                 if let user = user {
-                    self.completeSignIn(id: user.uid)
+                    let userData = ["provider": credential.provider]
+                    self.completeSignIn(id: user.uid, userData: userData)
                 }
             }
         })
@@ -73,7 +74,8 @@ class SignInVC: UIViewController {
                 if error == nil {
                     print("MAX : User authenticated with Firebase")
                     if let user = user {
-                        self.completeSignIn(id: user.uid)
+                        let userData = ["provider": user.providerID]
+                        self.completeSignIn(id: user.uid, userData: userData)
                     }
                 } else {
                     //Otherwise, create a new user
@@ -83,7 +85,8 @@ class SignInVC: UIViewController {
                         } else {
                             print("MAX : Successfully authenticated with Firebase")
                             if let user = user {
-                                self.completeSignIn(id: user.uid)
+                                let userData = ["provider": user.providerID]
+                                self.completeSignIn(id: user.uid, userData: userData)
                             }
                         }
                     })
@@ -92,8 +95,8 @@ class SignInVC: UIViewController {
         }
     }
     
-    func completeSignIn(id: String){
-        
+    func completeSignIn(id: String, userData: Dictionary<String, String>){
+        DataService.ds.createFirebaseDBUser(uid: id, userData: userData)
         let keychainResult = KeychainWrapper.standard.set(id, forKey: KEY_UID)
         print("MAX : Data saved to keychain \(keychainResult)")
         performSegue(withIdentifier: "goToFeed", sender: nil)
