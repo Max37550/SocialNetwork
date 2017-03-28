5c1fcd3c208e47fdd00cbc803da75bbc5512bc0b
Max37550 <maxime.peralez@hotmail.fr>
2017-03-28 19:25:34 +0200

    Completed adding image to a post

--- SocialNetwork/FeedVC.swift
+++ SocialNetwork/FeedVC.swift
@@ -10,17 +10,23 @@ import UIKit
 import Firebase
 import SwiftKeychainWrapper
 
-class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var addImage: CircleImageView!
     @IBOutlet weak var tableView: UITableView!
     
     var posts = [Post]()
+    var imagePicker : UIImagePickerController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
         tableView.delegate = self
         tableView.dataSource = self
+        
+        imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
         
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
             
@@ -59,7 +65,21 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
             return PostCell()
         }
     }
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+           addImage.image = image
+        } else {
+            print("MAX : A valid image wasn't selected")
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func addImagePressed(_ sender: Any) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
     @IBAction func signOutBtnPressed(_ sender: Any) {
 
         let keychainResult = KeychainWrapper.standard.removeObject(forKey: KEY_UID)
